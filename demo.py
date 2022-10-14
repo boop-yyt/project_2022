6034ac14cf1c1f575c68b1e2efd8b25f01984317
yuetingYang-boop <1018947841@qq.com>
2022-10-14 10:09:56 +0800

    demo update

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+from binhex import openrsrc
 from curses import init_pair
 import os
 from unittest import result
@@ -35,81 +36,90 @@ def extract_input_item(data):
     return puzzle, truth, fol_q, fol_a
 
 def get_response(prompt):
-	response = openai.Completion.create(
-		model="text-davinci-002",
-		prompt=prompt,
-		temperature=0.7,
-		max_tokens=100,
-		top_p=1,
-		frequency_penalty=0.0,
-		presence_penalty=0.0,
-		stop=["\n\n"]
-		)
-	return response.get('choices')[0]['text'].split("\n")[0]
-
-'''def get_response(prompt):
-    text = "okkkkkkkkkkkkkkkk"
-    return text
-'''
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n\n"]
+        )
+    output_ai = response.get('choices')[0]['text'].split("\n")[0]
+    if output_ai == "":
+        print("***** connect failure !! try again please.*****")
+    return output_ai
 
 def question_generation_input(data_item):
-    task_decription = ""
+    task_decription = "I am an intelligent bot that can play situation puzzles with user. A puzzle is given first, and the user begin to ask a \"yes/no\" question to ensure details."
+    # task_decription = "question generation"
     puzzle, truth, fol_q, fol_a = extract_input_item(data_item)
     example_prefix = "puzzle:" + "".join(example["puzzle"]) + "\n"
-    example_prefix += "follow_up_QA:"+example["follow-up-Q"][0]+example["follow-up-A"][0] + "\n"
+    example_prefix += "follow_up_Q:"+example["follow-up-Q"][0]+"follow_up_A:"+example["follow-up-A"][0] + "\n"
     example_prefix += "follow_up_Q:"+example["follow-up-Q"][1] + "\n"
     input_prefix = "puzzle:" + puzzle + "\n"  
-    input_prefix += "follow_up_QA:"
-    for fq,fa in zip(fol_q, fol_a):
-        input_prefix += fq + fa
-    input_prefix += "\n"+"follow_up_Q:"
+    if len(fol_q) and len(fol_a):
+        for fq,fa in zip(fol_q, fol_a):
+            input_prefix += "follow_ip_Q:"+ fq
+            input_prefix += "follow_ip_A:"+ fa
+        input_prefix += "\n"
+    input_prefix += "follow_up_Q:"
     final_prefix = task_decription + '\n' + example_prefix + input_prefix
     print("---------- question generation ----------\n")
     print(final_prefix)
     return final_prefix
 
 def answer_generation_input(data_item):
-    task_decription = ""
+    task_decription = "I am an intelligent bot that can play as judge in situation puzzles with user. A puzzle is given first, and the user begin to ask a \"yes/no\" question to ensure details, and I will give \"Yes/No/Irrelevent\" as answer to questions."
+    # task_decription = "answer generation"
     puzzle, truth, fol_q, fol_a = extract_input_item(data_item)
     example_prefix = "truth:" + "".join(example["truth"]) + "\n"
-    example_prefix += "follow_up_QA:"+example["follow-up-Q"][0]+example["follow-up-A"][0]+"\n"
-    example_prefix += "follow_up_Q:"+example["follow-up-Q"][1] + "\n"
-    example_prefix += "follow_up_A:"+example["follow-up-A"][1] + "\n"
+    example_prefix += "follow_up_Q:"+example["follow-up-Q"][0]+ "\n" +"follow_up_A:"+example["follow-up-A"][0] + "\n"
+    example_prefix += "follow_up_Q:"+example["follow-up-Q"][1]+ "\n" +"follow_up_A:"+example["follow-up-A"][1] + "\n"
     input_prefix = "truth:" + truth + "\n"  
-    input_prefix += "follow_up_QA:"
-    if fol_q and fol_a:
+    if len(fol_q) and len(fol_a):
         for fq,fa in zip(fol_q, fol_a):
-            input_prefix += fq + fa
-        input_prefix += "\n"+"follow_up_Q:" + data_item["follow-up-Q"][-1] 
+            input_prefix += "follow_ip_Q:"+ fq
+            input_prefix += "follow_ip_A:"+ fa
+        input_prefix += "\n"
+    input_prefix += "\n"+"follow_up_Q:" + data_item["follow-up-Q"][-1] 
     input_prefix += "\n" + "follow_up_A:"
     final_prefix = task_decription + '\n' + example_prefix + input_prefix
+    print("---------- answer generation ----------\n")
+    print(final_prefix)
     return final_prefix
 
 def solution_generate_input(data_item):
-    task_decription = ""
+    task_decription = "I am an intelligent bot that can play situation puzzles with user. A puzzle is given first, and the user begin to ask \"yes/no\" question to ensure details, then I will give the question a\"yes/no/irrelevent\" answer. Finally user try to give solution for the puzzle."
+    # task_decription = "solution generation"
     puzzle, truth, fol_q, fol_a = extract_input_item(data_item)
     example_prefix = "puzzle:" + "".join(example["puzzle"]) + "\n"
-    example_prefix += "follow_up_QA:"+example["follow-up-Q"][0]+example["follow-up-A"][0]+"\n"
+    example_prefix += "follow_up_Q:"+example["follow-up-Q"][0]+"follow_up_A:"+example["follow-up-A"][0] + "\n"
     example_prefix += "solution:" + "".join(example["truth"]) + "\n"
     input_prefix = "puzzle:" + puzzle + "\n"  
-    input_prefix += "follow_up_QA:"
-    if fol_q and fol_a:
+    if len(fol_q) and len(fol_a):
         for fq,fa in zip(fol_q, fol_a):
-            input_prefix += fq + fa
+            input_prefix += "follow_up_Q:"+ fq
+            input_prefix += "follow_up_A:"+ fa
+        input_prefix += "\n"
     input_prefix += "\n" + "solution:"
     final_prefix = task_decription + '\n' + example_prefix + input_prefix
+    print("---------- solution generation ----------\n")
+    print(final_prefix)
     return final_prefix
 
 if __name__=="__main__":
     parse = argparse.ArgumentParser()
-    parse.add_argument('--sample_num', type=int, default=1, help="the sample number to be infer")
-    parse.add_argument('--chance_num', type=int, default=2)
+    parse.add_argument('--sample_num', type=int, default=1)
+    parse.add_argument('--chance_num', type=int, default=1)
     parse.add_argument('--threshold', type=int, default=0.8)
     # parse.add_argument('--with_hint', action='store_true')
     args = parse.parse_args()
     dataset = load_data("./example.json")
     dataset = list(dataset.values())
-    output_file = "./Latral/output.txt"
+    output_file = "./output.txt"
+    output_dataset = "./new_dataset.json"
     # print(extract_input_item(dataset[0]))
     # print(question_generation_prompt(dataset[0]))
     # print(answer_generation_prompt(dataset[0]))
@@ -124,14 +134,18 @@ if __name__=="__main__":
             ditem["follow-up-Q"].append(generated_question)
             print("*",generated_question)
             # generate answer
-            # answer_generation_prompt = answer_generation_input(ditem)
-            # generated_answer = get_response(answer_generation_prompt)
-            # ditem["follow-up-A"].append(generated_answer)
-            # # generate solution
-            # solution_generate_prompt = solution_generate_input(ditem)
-            # generated_solution = get_response(solution_generate_prompt)
-            # print(f"the {chance_count+1} chance's answer:", generated_solution)
+            answer_generation_prompt = answer_generation_input(ditem)
+            generated_answer = get_response(answer_generation_prompt)
+            ditem["follow-up-A"].append(generated_answer)
+            print("*",generated_answer)
+            # generate solution
+            solution_generate_prompt = solution_generate_input(ditem)
+            generated_solution = get_response(solution_generate_prompt)
+            print(f"the {chance_count+1} chance's answer:", generated_solution)
             chance_count += 1
+        with open(output_dataset,"w",encoding="utf-8") as fd:
+            json.dump(dataset[:args.sample_num],fd)
+
             # calculate the current solution's accuracy
         #     similarity_score = compute_cosine_similarity("".join(data_item["truth"]), generated_solution)
         #     if similarity_score >= args.threshold:
